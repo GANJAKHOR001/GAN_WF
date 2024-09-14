e78ee4e7d41731e0b377fd833629ab7a8f3a874b
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-09-14 06:33:47 +0530

    Update database.py

--- shivu/database.py
+++ shivu/database.py
@@ -1,7 +1,7 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
 # Database connection setup
-mongo_url = "mongodb+srv://mongodbflex:L3GxCeWU3UcgFZZX-hwjiwjve@cluster0.czbo8.mongodb.net/?retryWrites=true&w=majority"
+mongo_url = "mongodb+srv://Epic2:w85NP8dEHmQxA5s7@cluster0.tttvsf9.mongodb.net/?retryWrites=true&w=majority"
 client = AsyncIOMotorClient(mongo_url)
 db = client['Character_catcher']
 sudo_users_collection = db['sudo_users_collection']
